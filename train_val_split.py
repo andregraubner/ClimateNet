b55915e9bd328a198fd3db0dadb2e71ba4513fd2
earens <earens@uos.de>
2022-10-10 17:27:40 +0200

    create val dataset

---
+++ train_val_split.py
@@ -0,0 +1,19 @@
+
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import shutil
+
+
+paths = np.array([f for f in listdir('data/train/') if isfile(join('data/train/', f))])
+idx = np.arange(len(paths))
+np.random.shuffle(idx)
+split = round(0.8*len(idx))
+
+train_paths = ['data/train/train/' + file for file in paths[idx[:split]]]
+val_paths = ['data/train/test/' + file for file in paths[idx[split:]]]
+
+dst_path = np.concatenate((train_paths, val_paths))
+
+for i, file in enumerate(paths):
+    shutil.move('data/train/'+file, dst_path[i])
