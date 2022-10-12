ce02b2fb381e2bd84ab213ad53d944168dbdaf07
Alexis Tabin <atabin@eu-login-39.euler.ethz.ch>
2022-10-12 10:51:15 +0200

    path fix for datasplit

--- train_val_split.py
+++ train_val_split.py
@@ -4,8 +4,8 @@ from os.path import isfile, join
 import numpy as np
 import shutil
 
-
-paths = np.array([f for f in listdir('data/train/') if isfile(join('data/train/', f))])
+print('stuff')
+paths = np.array([f for f in listdir('data/climatenet_new/train/') if isfile(join('data/train/', f))])
 idx = np.arange(len(paths))
 np.random.shuffle(idx)
 split = round(0.8*len(idx))
