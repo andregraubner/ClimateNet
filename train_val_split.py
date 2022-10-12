20cc4784516f842fa1bcc4538b8e663ae3d020dd
alexis <alexis.tabin@gmail.com>
2022-10-12 11:24:22 +0200

    Modify data path for data split

--- train_val_split.py
+++ train_val_split.py
@@ -4,16 +4,16 @@ from os.path import isfile, join
 import numpy as np
 import shutil
 
-print('stuff')
-paths = np.array([f for f in listdir('data/climatenet_new/train/') if isfile(join('data/train/', f))])
+DATASET_PATH = '/cluser/work/igp_psr/ai4good/group-1b/data/'
+paths = np.array([f for f in listdir(DATASET_PATH + 'train/') if isfile(join(DATASET_PATH + 'train/', f))])
 idx = np.arange(len(paths))
 np.random.shuffle(idx)
 split = round(0.8*len(idx))
 
-train_paths = ['data/train/train/' + file for file in paths[idx[:split]]]
-val_paths = ['data/train/test/' + file for file in paths[idx[split:]]]
+train_paths = [DATASET_PATH + '/train/train/' + file for file in paths[idx[:split]]]
+val_paths = [DATASET_PATH + '/train/test/' + file for file in paths[idx[split:]]]
 
 dst_path = np.concatenate((train_paths, val_paths))
 
 for i, file in enumerate(paths):
-    shutil.move('data/train/'+file, dst_path[i])
+    shutil.move(DATASET_PATH + '/train/'+file, dst_path[i])
