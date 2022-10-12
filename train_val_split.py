c84dc11ecba929e74a09a2820186ae25504df1cf
alexis <alexis.tabin@gmail.com>
2022-10-12 11:42:28 +0200

    add comments and security to data split

--- train_val_split.py
+++ train_val_split.py
@@ -5,6 +5,9 @@ import numpy as np
 import shutil
 from pathlib import Path
 
+IS_DATASET_SPLIT = True # True if dataset has already been split, false otherwise
+
+# CAREFUL : this script was already run once, so it won't do anything if you run it again
 DATASET_PATH = '/cluster/work/igp_psr/ai4good/group-1b/data/'
 paths = np.array([f for f in listdir(DATASET_PATH + 'train/') if isfile(join(DATASET_PATH + 'train/', f))])
 idx = np.arange(len(paths))
@@ -17,8 +20,9 @@ val_paths = [DATASET_PATH + 'train/' + file for file in paths[idx[split:]]]
 print('nb of train files : ', len(train_paths))
 print('nb of val files : ', len(val_paths))
 
-Path(DATASET_PATH + 'val/').mkdir(parents=True, exist_ok=True)
-for file in val_paths:
-    shutil.move(file, DATASET_PATH + 'val/')
+if not IS_DATASET_SPLIT :
+    Path(DATASET_PATH + 'val/').mkdir(parents=True, exist_ok=True)
+    for file in val_paths:
+        shutil.move(file, DATASET_PATH + 'val/')
 
 print('Done')
