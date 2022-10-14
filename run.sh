bc445cf66fdeb761026be91e964866e3d28e7989
alexis <alexis.tabin@gmail.com>
2022-10-14 13:24:50 +0200

    Add variable in shell script

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 source ~/.bashrc
 conda activate climatenet
 
-python ~/ClimateNet_AI4Good/run.py example \
+USER = "atabin"
+MODEL = "example"
+SCRATCH_FOLDER = "/cluster/scratch/$USER/"
+python ~/ClimateNet_AI4Good/run.py $MODEL \
     --data_dir /cluster/work/igp_psr/ai4good/group-1b/data/ \
-    --checkpoint_path /cluster/scratch/atabin/checkpoints/example/
+    --checkpoint_path $SCRATCH_FOLDER/checkpoints/$MODEL/
 
 echo "Done!"
