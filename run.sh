1e63f82046c3f0ecec8a6762419912fb1b8e98ac
alexis <alexis.tabin@gmail.com>
2022-10-14 13:47:35 +0200

    Fix bug in run script

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 source ~/.bashrc
 conda activate climatenet
 
-USER = "atabin"
-MODEL = "example"
-SCRATCH_FOLDER = "/cluster/scratch/$USER/"
-python ~/ClimateNet_AI4Good/run.py $MODEL \
+user="atabin"
+model="example"
+scratch_folder="/cluster/scratch/"
+echo "Running with following parameters : "
+echo $user
+echo $model
+echo $scratch_folder
+python ~/ClimateNet_AI4Good/run.py $model \
     --data_dir /cluster/work/igp_psr/ai4good/group-1b/data/ \
-    --checkpoint_path $SCRATCH_FOLDER/checkpoints/$MODEL/
+    --checkpoint_path $scratch_folder/checkpoints/$model/ \
+    --scratch_folder $scratch_folder \
+    --user $user
 
 echo "Done!"
