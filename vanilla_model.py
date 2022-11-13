f75937cb99e7722b45bf33aefa77b032b0508adb
earens <earens@student.ethz.ch>
2022-11-13 22:54:45 +0100

    small fixes

--- vanilla_model.py
+++ vanilla_model.py
@@ -85,7 +85,7 @@ class ImageDataset(Dataset):
         image = np.concatenate([np.array(data[var]) for var in self.var_list])
      
 
-        mask = torch.Tensor(np.array(data['LABELS']))
+        mask = torch.Tensor(np.array(data['LABELS']).astype(np.uint8))
 
     
 
