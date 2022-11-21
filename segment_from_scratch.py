d1e950ec8a882eec46e35d0fa04b7a9bbf21bbf2
earens <earens@student.ethz.ch>
2022-11-21 18:14:35 +0100

    small fixes

--- segment_from_scratch.py
+++ segment_from_scratch.py
@@ -172,8 +172,8 @@ class Model_Task(SemanticSegmentationTask):
         loss = self.loss(y_hat, y) 
         self.log("val_loss", loss, on_step=False, on_epoch=True)
 
-        y_numpy = y.cpu().numpy()
-        y_hat_int_numpy = y_hat_int.cpu().numpy()
+        y_numpy = y.gpu().numpy()
+        y_hat_int_numpy = y_hat_int.gpu().numpy()
 
 
         for y_hat_i, y_i in zip(y_hat_int_numpy,y_numpy):
