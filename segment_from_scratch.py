8c334350bddb6984a7d6adf403f5e6fbb24acaa9
earens <earens@student.ethz.ch>
2022-11-21 11:48:59 +0100

    small fixes

--- segment_from_scratch.py
+++ segment_from_scratch.py
@@ -180,8 +180,8 @@ class Data(LightningDataModule):
         return test_dataloader
 
 
-class Model(SemanticSegmentationTask):
-    def __init__(self, path):
+class Model_Task(SemanticSegmentationTask):
+    def __init__(self):
         super().__init__()
     
  
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     wandb_logger = WandbLogger(entity="ai4good", log_model=True, project="segment_from_scratch")
 
     # set up task
-    task = Model(
+    task = Model_Task(
         segmentation_model=conf["model"]["segmentation_model"],
         encoder_name=conf["model"]["backbone"],
         encoder_weights="imagenet" if conf["model"]["pretrained"] == "True" else "None",
