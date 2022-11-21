53ee8e9fb99d48942873e2604b5bcc89f4dd8f88
earens <earens@student.ethz.ch>
2022-11-21 17:20:27 +0100

    small fixes

--- segment_from_scratch.py
+++ segment_from_scratch.py
@@ -175,11 +175,11 @@ class Model_Task(SemanticSegmentationTask):
 
             image = wandb.Image(bg_im, masks={
             "predictions" : {
-                "mask_data" : torch.tensor(y_hat_i.argmax(dim=1)),
+                "mask_data" : np.array(y_hat_i.argmax(dim=1)),
                 "class_labels" : class_labels
             },
             "ground_truth" : {
-                "mask_data" :torch.tensor(y_i),
+                "mask_data" :np.array(y_i),
                 "class_labels" : class_labels
             }
             })
