cc2f969c70bb6974163b4e574c30f2c2a0f59eb1
lhendre <lhendre2@github.com>
2022-11-25 18:02:05 -0600

    adding in random -180 to 180 lon shift

--- climatenet/dataAugmentator.py
+++ climatenet/dataAugmentator.py
@@ -1,8 +1,9 @@
 import netCDF4 as nc
 import os
+import random
 
 data_path = '/Users/lucashendren/workspace/cs230/Data-4/Train'
-dims_to_change = ["lon","lat"]
+dims_to_change = ["lon"]
 
 for filename in os.listdir(data_path):
     f = os.path.join(data_path, filename)
@@ -28,7 +29,14 @@ for filename in os.listdir(data_path):
             values = ds[name][:]
             if name in dims_to_change:
                 for i,val in enumerate(values):
-                    values[i]=val+1
+                    toAdd = random.randint(-180, 180)
+                    newVal=val+toAdd
+                    if newVal < -180:
+                        newVal= 360+newVal
+                    if newVal > 180:
+                        newVal= newVal-360
+                    values[i]=newVal
+
             newDs[name][:] = values
             if name in dims_to_change:
                 print("old",ds.variables[name][0])
