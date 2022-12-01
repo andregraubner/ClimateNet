54b7e6c213d42d150e4ae5e9ed300a2b6dda0b2c
earens <earens@student.ethz.ch>
2022-12-01 19:55:19 +0100

    fixes

--- segment_from_scratch_cl.py
+++ segment_from_scratch_cl.py
@@ -367,7 +367,7 @@ if __name__ == "__main__":
                 callbacks=[checkpoint_callback, early_stopping_callback],
                 logger=[csv_logger, wandb_logger],
                 accelerator="gpu",
-                max_epochs=int(conf["trainer"]["max_epochs"])*(i+1),
+                max_epochs=int(conf["trainer"]["max_epochs"])*2,
                 max_time=conf["trainer"]["max_time"],
                 auto_lr_find=conf["trainer"]["auto_lr_find"] == "True",
                 auto_scale_batch_size=conf["trainer"]["auto_scale_batch_size"] == "True",
