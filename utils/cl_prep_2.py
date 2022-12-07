b643be83a03cf6419b3e9f75d0330405d5dff471
earens <earens@student.ethz.ch>
2022-12-07 17:39:37 +0100

    fixes

--- utils/cl_prep_2.py
+++ utils/cl_prep_2.py
@@ -228,7 +228,6 @@ def save_best_patches(set, vars,file_name, image, im_patches, class_freq, max_ex
             xr_patch = xr.Dataset(data_vars=data_vars, coords=coords)
             
             xr_patch.to_netcdf(os.path.join(paths[i]+str(set)+'_'+file_name+"_p"+str(n)+".nc"))
-            print(os.path.join(paths[i]+str(set)+'_'+file_name+"_p"+str(n)+".nc"))
             xr_patch.close()
 
 def save_best_patches_test(set, vars,file_name, image, im_patches, class_freq, max_exp_patches):
@@ -282,7 +281,6 @@ def save_best_patches_test(set, vars,file_name, image, im_patches, class_freq, m
         data_vars["LABELS"] = (['lat', 'lon'], save_patch[0,:,:].astype(np.int64))
 
         xr_patch = xr.Dataset(data_vars=data_vars, coords=coords)
-        print(os.path.join(path_test+file_name+"_p"+str(n)+".nc"))
         xr_patch.to_netcdf(os.path.join(path_test+file_name+"_p"+str(n)+".nc"))
         xr_patch.close()
 
@@ -312,7 +310,7 @@ def process_all_images(patch_size, stride, vars, max_exp_patches):
         file_names = [f[:-3] for f in listdir(data_dir) if isfile(join(data_dir, f))]
         print('Load all images')
         data = []
-        for p in tqdm(single_file_paths[:2]):
+        for p in tqdm(single_file_paths[:]):
             try:
                 data.append(xr.load_dataset(p))
             except:
